c473f0bd98709ab41b6322e01e1109870798fb38
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2018-11-30 16:37:05 +0100

    Update

--- utils/stuff.py
+++ utils/stuff.py
@@ -4,10 +4,23 @@
 
 import numpy as np
 import logging
+from enum import IntFlag
 
 from scipy.spatial import KDTree
 
 logger = logging.getLogger(__name__)
+
+class SourceFlags(IntFlag):
+    """
+    SExtractor++ flags
+    """
+    NONE      = 0 # No flag is set
+    BIASED    = 1 << 0 # The object has bad pixels
+    BLENDED   = 1 << 1 # The object was originally blended with another one.
+    SATURATED = 1 << 2 # At least one pixel of the object is saturated.
+    BOUNDARY  = 1 << 3 # The object is truncates (to close to an image boundary).
+    NEIGHBORS = 1 << 4 # The object has neighbors, bright and close enough
+    OUTSIDE   = 1 << 5 # The object is completely outside of the measurement frame
 
 
 def parse_stuff_list(path):
